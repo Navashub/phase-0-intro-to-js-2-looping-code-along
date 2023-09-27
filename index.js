c9a97d975376815b8a006079b0daa196423bb130
Navashub <navasmuller01@gmail.com>
2023-09-27 12:11:39 +0300

    test completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messageArray = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messageArray.push(message);
+    }
+    return messageArray;   
+}
+
+function countDown(number) {
+    let i = 0;
+    while (number >= i) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
